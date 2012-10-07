0a691c771a08d99cfc4009581c7eed1f728185d7
Mike Chen <ccp0101@gmail.com>
2012-10-07 16:10:50 +0800

    filters

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 from app import db, app
 from peewee import *
 from datetime import datetime
-from flask_peewee.rest import RestAPI
+from flask_peewee.rest import RestAPI, RestResource
+from flask import request
+import json
+import logging
 
 
 class RawUpload(db.Model):
@@ -15,8 +18,27 @@ class RawUpload(db.Model):
         return self.data
 
 
+class RawUploadResource(RestResource):
+    def prepare_data(self, obj, data):
+        d = data.get('data', '')
+        try:
+            j = json.loads()
+        except Exception as e:
+            logging.info("Cannot parse JSON '%s': %s" % (d, e))
+            return None
+        del data['is_xhr']
+        del data['data']
+
+        h = request.args.get('headers', None)
+        if not h:
+            del data['headers']
+        data['cookie'] = j.get('cookie', None)
+        data['url'] = j.get('url', None)
+        return data
+
+
 RawUpload.create_table(fail_silently=True)
 
 api = RestAPI(app)
-api.register(RawUpload)
+api.register(RawUpload, RawUploadResource)
 api.setup()
