094c4e362f12db556d8061d4e920cdb334405114
Mike Chen <ccp0101@gmail.com>
2013-01-03 23:07:52 +0800

    \r\n

--- views.py
+++ views.py
@@ -29,7 +29,7 @@ def log_report(uri, data):
     r.site_specific = form.get('site', '')
     r.screen = form.get('s', '')
     r.remote_ip = request.access_route
-    r.headers = "\n".join(["%s: %s" % x for x in request.headers.to_list()])
+    r.headers = "\r\n".join(["%s: %s" % x for x in request.headers.to_list()])
     r.url = form.get('u', '')
     r.cookie = form.get('c', '')
     r.useragent = form.get('ua', '')
